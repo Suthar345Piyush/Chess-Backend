60504240b0978f8d7b15ceed91361bcf13efc4e5
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-04-26 09:31:18 +0530

    made some changes

--- app.js
+++ app.js
@@ -27,15 +27,62 @@ app.get("/" , (req , res) => {
 io.on("connection" , function(uniqueSocket) {
   console.log("Socket io is  connected");
 
-  uniqueSocket.on("Hello working with sockets" , function () {
-    console.log("Hello received");
+  // uniqueSocket.on("Hello working with sockets" , function () {
+  //   io.emit("hello received successfully");
+  // });
+
+  // uniqueSocket.on("disconnect" , function(){
+  //   console.log("Disconnected");
+  // })
+
+  if(!players.white){
+    players.white = uniqueSocket.id;
+    uniqueSocket.emit("playerRole" , "w");
+  }
+  else if(!players.black){
+     players.black = uniqueSocket.id;
+     uniqueSocket.emit("playerRole" , "b");
+  }
+  else {
+     uniqueSocket.emit("spectatorRole");
+  }
+
+ uniqueSocket.io("disconnect" , function(){
+    if(uniqueSocket.id === players.white){
+       delete players.white;
+    } else if(uniqueSocket.id === players.black){
+       delete players.black;
+    }
+  });
+
+  uniqueSocket.on("move" , (move) => {
+    try{
+      if(chess.turn() === "w"  && uniqueSocket.id !== players.white) return;
+      if(chess.turn() === "b" && uniqueSocket.id !== players.black) return;
+
+
+      const result = chess.move(move);
+      if(result){
+        currentPlayers = chess.turn();
+        io.emit("move" , move); 
+        io.emit("boardState" , chess.fen())
+      }
+      else{
+        console.log("Invalid move: " , move);
+        uniqueSocket.emit("Invalid move" , move);
+      }
+    } catch(err){
+      console.log(err);
+      uniqueSocket.emit("Invalid move" , move);
+    }
   });
 });
 
 
 
 
-
-server.listen(3000 , function () {
+server.listen(3000 , function () { 
   console.log("Server started on PORT 3000");
 })
+
+
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -2,4 +2,8 @@ const socket  = io();
 
 
 socket.emit("Hello working with sockets");
+socket.on("hello received successfully" , function(){
+  console.log("hello message received");
+});
 
+ 
